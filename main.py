b392ff07e9271dd1167c6e8d848e9fdd824fa341
Sujal Negi <105803136+sujalnegi@users.noreply.github.com>
2025-10-07 16:10:20 +0530

    #Commit 8: Face Cropper with Blur, Resolution, DNN Model & Manual Review Checks
    The face extractor now uses a three-step quality filter: it first auto-discards faces that are too small (low resolution), then auto-rejects blurry photos using a sharpness check, and finally, auto-validates crops with a high-accuracy DNN model. Only images that pass all automated quality gates are sent to the user for the final manual save or discard choice.

--- main.py
+++ main.py
@@ -1,17 +1,68 @@
 import cv2
 import os
+import numpy as np
+import sys
 
 # --- Configuration ---
 IMAGE_PATH = 'src-img\\t1.jpg'  
 OUTPUT_DIR = 'out-img'
 CASCADE_PATH = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
 
+# DNN Model Files
+PROTOTXT_PATH = 'deploy.prototxt.txt'
+CAFFEMODEL_PATH = 'res10_300x300_ssd_iter_140000.caffemodel'
+CONFIDENCE_THRESHOLD = 0.5
 
+# Quality Filters
+MIN_CROP_DIMENSION = 80
+
+# Blur detection function
+# Blur Detection Constant (10 to 300)
+BLUR_THRESHOLD = 150.
+
+
+def is_blurry(image, threshold=BLUR_THRESHOLD):
+    if image is None or image.size == 0:
+        return True
+    
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    laplacian_var = cv2.Laplacian(gray, cv2.CV_64F).var()
+    return laplacian_var < threshold, laplacian_var
+
+# Detection Setup
 if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
     print(f"Created output directory: {OUTPUT_DIR}")
 
-# Loading the cascade classifier
+try:
+    net = cv2.dnn.readNetFromCaffe(PROTOTXT_PATH, CAFFEMODEL_PATH)
+except:
+    print("Could not load DNN model files.")
+    print("Please ensure 'deploy.prototxt' and 'res10_300x300_ssd_iter_140000.caffemodel' are downloaded.")
+    sys.exit()
+
+def recheck_face_validity(cropped_face_img, dnn_net, confidence_threshold=CONFIDENCE_THRESHOLD):
+    if cropped_face_img is None or cropped_face_img.size == 0:
+        return False, 0.0
+    
+    (h, w) = cropped_face_img.shape[:2]
+
+    blob = cv2.dnn.blobFromImage(cv2.resize(cropped_face_img, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+    dnn_net.setInput(blob)
+    detections = dnn_net.forward()
+
+    max_confidence = 0.0
+
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+    
+        if confidence > max_confidence:
+            max_confidence = confidence
+    # returns true when max confidence if more than the set threshold
+    return max_confidence >= confidence_threshold, max_confidence
+
+
+# Loading the cascade classifier    
 face_cascade = cv2.CascadeClassifier(CASCADE_PATH)
 
 if face_cascade.empty():
@@ -37,6 +88,9 @@ faces = face_cascade.detectMultiScale(
 )
 
 total_detected = len(faces)
+auto_rejected_count = 0
+auto_rejected_dnn=0
+auto_rejected_small=0
 saved_count = 0
 print(f"Detected {total_detected} potential faces. Starting interactive review...")
 
@@ -45,6 +99,27 @@ for i, (x, y, w, h) in enumerate(faces):
     # Crop the face region from the *original color image*
     cropped_face = image[y:y + h, x:x + w]
     
+    # 1.Automatic Resolution check
+    if w < MIN_CROP_DIMENSION or h < MIN_CROP_DIMENSION:
+        auto_rejected_count += 1
+        print(f"[AUTO REJECTED] Face {i+1} too small (WxJ: {w}x{h}). Skipping...")
+        continue
+    
+    # 2. Blur check
+    is_blurry_flag, variance = is_blurry(cropped_face, BLUR_THRESHOLD)
+    if is_blurry_flag:
+        auto_rejected_count += 1
+        print(f"[AUTO REJECTED] Face {i+1} is blurry (Variance: {variance: 2f}). Skipping...")
+        continue
+
+    # 3. DNN Recheck
+    is_valid_face, dnn_confidence = recheck_face_validity(cropped_face, net, CONFIDENCE_THRESHOLD)
+    if not is_valid_face:
+        auto_rejected_dnn += 1
+        print(f"[AUTO REJECTED] Face {i+1} failed DNN recheck (Max Confidence: {dnn_confidence*100:.2f}%). Skipping...")
+        continue
+
+    # 4. Maual Review
     # Create a unique window for the cropped face
     window_name = f"Face {i+1}/{total_detected} - [S]ave or [D]iscard"
     cv2.imshow(window_name, cropped_face)
@@ -81,4 +156,12 @@ for i, (x, y, w, h) in enumerate(faces):
 
 cv2.destroyAllWindows()
 print("-" * 30)
-print(f"Review complete. Total detected: {total_detected}. Total saved: {saved_count}")
+print(f"Review complete.")
+print(f"Total detected: {total_detected}.")
+print(f"Auto-rejected (too small): {auto_rejected_small}.")
+print(f"Auto-rejected (blurry): {auto_rejected_count}.")
+print(f"Auto-rejected (DNN fail): {auto_rejected_dnn}.")
+print(f"Manually Reviewed: {total_detected-auto_rejected_count-auto_rejected_dnn-auto_rejected_small}.")
+print(f"Total saved: {saved_count}")   
+
+
